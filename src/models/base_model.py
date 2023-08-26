abaaf88beae00129fccc115deaff94bf152f75d5
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-08-26 08:39:50 +0200

    feat: base models for other models

--- src/models/base_model.py
+++ src/models/base_model.py
@@ -8,13 +8,13 @@ class BaseModel:
     """"""
     def __init__(self, *args, **kwargs):
         """base model constructor"""
-        id = uuid4()
-        created_at = datetime.now()
-        updated_at = datetime.now()
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """to string method"""
-        print("[{}] ({}) {}".format(self.__class__, self.id, self.__dict__))
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """default save method"""
