4ba9b75bbfe2dfe164c42395c2f520ac2a2a43b3
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-08-29 17:39:44 +0200

    add dotenv module to control environment variables

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ Tickethub app backend
 
 1. First you need to have the python3 installed on your machine
 2. For the database you need to have the postgres database
-2. Install flask, sqlalchemy, psyco2
+2. Install flask, sqlalchemy, psyco2, dotenv
--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,7 +1,10 @@
 # !/usr/bin/python3
+from dotenv import load_dotenv
 from models.base_model import BaseModel, Base
 from models.organisation import Organisation
 from models.engine.db_storage import DBStorage
+
+load_dotenv()
 
 storage = DBStorage()
 storage.reload()
