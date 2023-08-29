a873c537247099db1bab33f466295aafe6694b79
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-08-29 22:28:23 +0200

    feat: handling errors

--- src/api/v1/app.py
+++ src/api/v1/app.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
 """Main Entrance for the api"""
-from flask import Flask
+from flask import Flask, jsonify
+from models import storage
 from api.v1.routes import app_routes
 
 app = Flask(__name__)
 
 app.register_blueprint(app_routes)
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """Close the session"""
+    storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handling the 404 error"""
+    return jsonify({"status": "not found"}), 404
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
